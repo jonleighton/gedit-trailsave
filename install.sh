919d52797b1cac46063163f1703437ad77e9cdf6
Jon Leighton <j@jonathanleighton.com>
2011-06-20 23:23:02 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mkdir -p ~/.local/share/gedit/plugins
+cp trailsave.* ~/.local/share/gedit/plugins
+
+echo "Trailsave installed! Now restart Gedit and active the plugin in Edit > Preferences."
